bd69c1f53374ec81a3ab506bf6874f9869c73086
David Boedigheimer <stealth259@gmail.com>
2021-03-16 15:30:36 -0700

    Add change to engineer in database.

---
+++ Factory/Migrations/20210316204339_Description.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Factory.Migrations
+{
+    public partial class Description : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "License",
+                table: "Engineers",
+                newName: "Description");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Engineers",
+                newName: "License");
+        }
+    }
+}
